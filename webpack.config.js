4600982bf9c7e90507b2d08ba780623b2d4f4fba
Fadillah Hadiyanto <fadillah.it@gmail.com>
2018-02-23 15:37:04 +0700

    Add Foundation & jQuery

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
+var webpack = require('webpack');
 module.exports = {
-    entry: './app/app.jsx',
+    entry: [
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx',
+    ],
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
     output: {
         path: __dirname,
         filename: './public/bundle.js'
     },
     resolve: {
         root: __dirname,
-        alias:{
+        alias: {
             Main: 'app/components/Main.jsx',
             Nav: 'app/components/Nav.jsx',
             Weather: 'app/components/Weather.jsx',
